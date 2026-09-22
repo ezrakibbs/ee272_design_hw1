376f507745e96b533cf337ff4478532c419762fd
ezrakibbs <ezrazeleke@gmail.com>
2026-09-21 20:05:05 -0700

    version 1.0
    - works using different inputs tested in testbench
    - going to try Professor Jones' tb

---
+++ fpm.sv
@@ -0,0 +1,201 @@
+// VERSION: 1.0
+
+/*
+Design a floating point multiplier.  This are inspired by IEEE 754-2019 (See the SJSU library to download the specification)
+
+The Floating point is 1 bit sign, 5 bits exponent biased at +15 (6'b01111), and 6 bits fraction (with a hidden 1, 7 effective bits)
+
+The multiplier and adder do not generate overflow or underflow, but "saturate" at +/- 11110_110000. Below 00010_000000 are set to true zero.
+
+There is no need for NAN or infinities in the design due to the saturation applied at all steps.
+
+A true Zero is represented by all zero bits in the exponent and fraction. Any other coding of the 12 bits represents a non zero value.
+
+A true Zero is positive (sign bit is zero) when created by the multiplier. (-0 becomes 0)
+
+You can find the test bench in ~morris/272/f25/hw1There is also a header file that shows how the test bench expects the interface.The test generation and checking are encrypted to keep you focused on original design.
+
+*/
+
+// 12 bit floating point multiplier module
+//  - 1 sign
+//  - 5 exponent
+//  - 6 mantissa
+
+module fpm(input  reg clk,
+           input  reg reset,
+           input  reg [11:0] A,
+           input  reg [11:0] B,
+           input  reg pushin,
+           output reg pushout,
+           output reg [11:0] Z
+);
+
+  localparam SATURATIOn = 11'b11110_110000;
+  localparam ROUND_TO_ZERo = 11'b00010_000000;
+
+  typedef struct packed{
+    logic signbit;
+    logic [4:0] exponent;
+    logic [5:0] mantissa;
+  } fp_12b;
+
+  fp_12b a;
+  fp_12b b;
+
+  assign a = A;
+  assign b = B;
+
+  // stage 1 - multiply
+  reg s1_signbit;
+  reg [5:0] s1_unnormal_exponent;
+  reg [13:0] s1_unnormal_mantissa; // 7bits * 7bits can be up to 14 bits
+  reg s1_pushout;
+  logic detect_zero_flag;
+
+  always@(*) begin
+    if(({a.signbit, a.exponent, a.mantissa} == 0) | ({b.signbit, b.exponent, b.mantissa} == 0)) begin
+      detect_zero_flag = 1;
+    end
+    else begin
+      detect_zero_flag = 0;
+    end
+  end
+
+  always@(posedge clk) begin
+    if(reset | detect_zero_flag | !pushin)
+    begin
+      s1_signbit <= 0;
+      s1_unnormal_exponent <= 0;
+      s1_unnormal_mantissa <= 0;
+      s1_pushout <= 0;
+    end
+
+    else 
+    begin
+      s1_signbit <= a.signbit ^ b.signbit;
+      s1_unnormal_exponent <= a.exponent + b.exponent - 15;
+      s1_unnormal_mantissa <= ({1'b1, a.mantissa}) * ({1'b1, b.mantissa});
+      s1_pushout <= 1;
+    end
+  end
+
+  // stage 2 - normalize
+  reg s2_signbit;
+  reg [5:0] s2_exponent;
+  reg [5:0] s2_mantissa; // mantissa should be 6 bits after normalization
+  reg s2_pushout;
+
+  logic [3:0] position_first_one;
+  logic found_first_one;
+  logic [5:0] normal_mantissa;
+  logic [13:0] shifted_mantissa;
+  logic [5:0] unnormal_exponent;
+  logic [5:0] normal_exponent;
+  logic [2:0] flag;
+  always@(*) begin  // combinational logic to normalize mantissa
+    if((s1_unnormal_mantissa == 0) && (s1_unnormal_exponent == 0))begin
+      position_first_one = 0;
+      found_first_one = 0;
+      normal_mantissa = 0;
+      shifted_mantissa = 0;
+      unnormal_exponent = 0;
+      flag = 0;
+    end
+
+    else begin
+      for (int i = 13; i >= 0; i--) begin
+        if (!found_first_one && s1_unnormal_mantissa[i]) begin
+            position_first_one = i;
+            found_first_one = 1;
+        end
+      end
+
+      if(position_first_one >= 6) begin
+        normal_mantissa = s1_unnormal_mantissa[position_first_one - 1 -:6];
+        shifted_mantissa = 0;
+      end
+      else begin
+        shifted_mantissa = s1_unnormal_mantissa << (6 - position_first_one);
+        normal_mantissa = shifted_mantissa[5:0];
+      end
+
+      if({s1_unnormal_exponent, normal_mantissa} > SATURATIOn) begin
+        normal_exponent = SATURATIOn[10:6];
+        normal_mantissa = SATURATIOn[5:0];
+      end
+
+      else begin
+        if(position_first_one > s1_unnormal_exponent) begin
+          unnormal_exponent = s1_unnormal_exponent + (position_first_one - s1_unnormal_exponent);
+          flag = 1;
+        end
+        else if(position_first_one < s1_unnormal_exponent) begin
+          unnormal_exponent = s1_unnormal_exponent - (s1_unnormal_exponent - position_first_one);
+          flag = 2;
+        end
+        else begin
+          unnormal_exponent = s1_unnormal_exponent;
+          flag = 3;
+        end
+
+        if({unnormal_exponent, normal_mantissa} > SATURATIOn) begin
+          normal_exponent = SATURATIOn[10:6];
+          normal_mantissa = SATURATIOn[5:0];
+        end
+        else begin
+          normal_exponent = unnormal_exponent[4:0];
+        end
+      end
+    end
+
+
+  end
+
+  always@(posedge clk) begin
+    if(reset | !s1_pushout)
+    begin
+      s2_signbit <= 0;
+      s2_exponent <= 0;
+      s2_mantissa <= 0;
+      s2_pushout <= 0;
+    end
+
+    else begin
+      s2_signbit <= s1_signbit;
+      s2_exponent <= normal_exponent;
+      s2_mantissa <= normal_mantissa;
+      s2_pushout <= 1;
+    end
+  end
+
+  // stage 3 - handle exceptions
+  logic result_is_zero;
+  
+  always@(posedge clk) begin
+    if(reset | !s2_pushout)
+    begin
+      Z <= 0;
+      pushout <= 0;
+    end
+
+    else if((s2_exponent == 0) & (s2_mantissa == 0)) begin
+      Z <= 0;
+      pushout <= 1;
+    end
+
+    else if({s2_exponent, s2_mantissa} < ROUND_TO_ZERo) begin
+      Z <= 0;
+      pushout <= 1;
+    end
+
+    else begin
+      Z[11] <= s2_signbit;
+      Z[10:6] <= s2_exponent;
+      Z[5:0] <= s2_mantissa;
+      pushout <= 1;
+    end
+  end
+
+endmodule : fpm
+
---
+++ fpm_tb.sv
@@ -0,0 +1,58 @@
+`timescale 1ns / 1ps
+module fpm_tb;
+  reg clk;
+  reg reset;
+  reg [11:0] input_1;
+  reg [11:0] input_2;
+  reg pushin;
+  wire pushout;
+  wire [11:0] output_1;
+
+
+  fpm test(
+      .clk(clk),
+      .reset(reset),
+      .A(input_1), 
+      .B(input_2),
+      .pushin(pushin),
+      .pushout(pushout),
+      .Z(output_1));
+  
+  initial begin
+    clk = 0;
+    reset = 1;
+    #15
+    reset = 0;
+    pushin = 0;
+  end
+
+  always #10 clk = ~clk;
+
+  initial begin
+    #25
+    input_1 = 12'b0_10101_100011; // 99
+    input_2 = 12'b1_10101_011001; // -89
+    pushin = 1;
+    #20
+    input_1 = 12'b1_10101_101110; // -110.125
+    input_2 = 12'b0_10101_100011; // 99.875
+    #20
+    input_1 = 12'b0_00000_000000; // 0
+    input_2 = 12'b1_10101_100011; // -99
+    #20
+    input_1 = 12'b0_11111_000000;
+    input_2 = 12'b1_10111_100011; 
+
+  end
+
+  initial begin
+    #200
+    $finish();
+  end
+
+
+  
+
+
+endmodule : fpm_tb
+
